d8becd515a9f2b33d51dc159c62b6eb3707ba235
Kosmas Sourlis <kosmassourlis25@gmail.com>
2022-04-11 23:52:32 +0300

    centrer image

--- DroneSideCode/GUI.py
+++ DroneSideCode/GUI.py
@@ -6,14 +6,14 @@ from PIL import ImageTk
 root = Tk()
 width ,height = 900,600
 root.minsize(width,height)
-frm = ttk.Frame(root, padding=width /2)
+
+frm = ttk.Frame(root, padding=200)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 
 image1 = Image.open("Recources\Images\photo.jpg")
 test = ImageTk.PhotoImage(image1)
-
 label1 = ttk.Label(image=test)
 label1.image = test
 
