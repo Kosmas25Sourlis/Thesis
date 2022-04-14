dc11bd5c4748b44a90f7cd9b5a4f140148149aa7
Kosmas Sourlis <kosmassourlis25@gmail.com>
2022-04-14 11:22:02 +0300

    database insertion complete lets make some records

--- DroneSideCode/Recources/Database/InsertDatabase.sql
+++ DroneSideCode/Recources/Database/InsertDatabase.sql
@@ -1,5 +1,33 @@
-CREATE DATABAse IF NOT EXISTS `crimes`;
-  CREATE TABLE IF NOT EXISTS `crimes`.`people` (
+CREATE DATABASE IF NOT EXISTS `crimes`;
+
+CREATE TABLE IF NOT EXISTS `crimes`.`Zones` (
+`Zone_ID` int NOT NULL AUTO_INCREMENT,
+`Zone` VARCHAR(16) NOT NULL,
+PRIMARY KEY (`Zone_ID`)
+);
+
+CREATE TABLE IF NOT EXISTS `crimes`.`Crime_area` (
+  `Area_ID` int NOT NULL AUTO_INCREMENT,
+  `Zone_ID` int NOT NULL,
+  `Street` VARCHAR(80) NOT NULL,
+  `AreaDescription` VARCHAR(80) NOT NULL,
+  PRIMARY KEY (`Area_ID`),
+  CONSTRAINT `FKAreaID`
+  FOREIGN KEY (`Zone_ID`)
+  REFERENCES `crimes`.`Zones` (`Zone_ID`)
+  );
+
+
+CREATE TABLE IF NOT EXISTS `crimes`.`People_addresses` (
+`AddressID` INT NOT NULL AUTO_INCREMENT,
+`Country` VARCHAR(300) NOT NULL,
+`Town` VARCHAR(100) NOT NULL,
+`ZipCode` VARCHAR(100) NOT NULL,
+`Street` VARCHAR(100) NOT NULL,
+`Type` VARCHAR(50) NOT NULL,
+PRIMARY KEY (`AddressID`)
+);
+CREATE TABLE IF NOT EXISTS `crimes`.`People` (
   `Person_ID` int NOT NULL AUTO_INCREMENT,
   `Surname` VARCHAR(80) NOT NULL,
   `First_name` VARCHAR(80) NOT NULL,
@@ -11,100 +39,56 @@ CREATE DATABAse IF NOT EXISTS `crimes`;
   `PhotoURL` VARCHAR(80) NOT NULL,
   `Years_in_jail` VARCHAR(30),
   `AddressID` INT NOT NULL,
-  PRIMARY KEY (`Person_ID`)
+  PRIMARY KEY (`Person_ID`),
+  CONSTRAINT `FKAddress_ID`
+  FOREIGN KEY (`AddressID`)
+  REFERENCES `crimes`.`People_addresses` (`AddressID`)
+  );
+
+CREATE TABLE IF NOT EXISTS `crimes`.`Crime_records` (
+`CrimeID` int NOT NULL AUTO_INCREMENT,
+`Crime_description` VARCHAR(300) NOT NULL,
+`Area_ID` int NOT NULL,
+`Date` VARCHAR(300) NOT NULL,
+`Time` VARCHAR(300) NOT NULL,
+`Additional info` VARCHAR(300),
+  PRIMARY KEY (`CrimeID`),
+  CONSTRAINT `FKArea_ID`
+  FOREIGN KEY (`Area_ID`)
+  REFERENCES `crimes`.`Crime_area` (`Area_ID`)
   );
 
 
-
-
-CREATE TABLE IF NOT EXISTS `crimes`.`Zones` (
-`Zone_ID` int NOT NULL ,
-`Zone` VARCHAR(16) NOT NULL,
-PRIMARY KEY (`Zone_ID`)
-);
-
-CREATE TABLE IF NOT EXISTS `crimes`.`Crime_area` (
-  `Area_ID` int NOT NULL AUTO_INCREMENT,
-  `Zone_ID` VARCHAR(16) NOT NULL,
-  `Street` VARCHAR(80) NOT NULL,
-  `AreaDescription` VARCHAR(80) NOT NULL,
-  PRIMARY KEY (`Area_ID`),
-
-  CONSTRAINT `FKAreaID`
-    FOREIGN  (`Zone_ID`)
-    REFERENCES `crimes`.`Zones` (`Zone_ID`)
+CREATE TABLE IF NOT EXISTS `crimes`.`Victims` (
+  `VictimID` int NOT NULL AUTO_INCREMENT,
+  `CrimeID` int NOT NULL,
+  PRIMARY KEY (`VictimID`),
+  CONSTRAINT `FKVictimID`
+  FOREIGN KEY (`VictimID`)
+  REFERENCES `crimes`.`People` (`Person_ID`),
+  CONSTRAINT `FKCrime_ID2`
+  FOREIGN KEY (`CrimeID`)
+  REFERENCES `crimes`.`Crime_records` (`CrimeID`)
   );
-
 
 CREATE TABLE IF NOT EXISTS `crimes`.`Criminals` (
-  `CriminalID` VARCHAR(16) NOT NULL,
-  `CrimeID` VARCHAR(16) NOT NULL ,
-  PRIMARY KEY (`CriminalID`)
-  );
-CREATE TABLE IF NOT EXISTS `crimes`.`Victims` (
-  `VictimID` VARCHAR(16) NOT NULL,
-  `CrimeID` VARCHAR(16) NOT NULL ,
-  PRIMARY KEY (`VictimID`)
-  );
-    CREATE TABLE IF NOT EXISTS `crimes`.`People_Addresses` (
-  `AddressID` int NOT NULL AUTO_INCREMENT,
-  `Country` VARCHAR(300) NOT NULL,
-  `Town` VARCHAR(100) NOT NULL,
-  `ZipCode` VARCHAR(100) NOT NULL,
-  `Street` VARCHAR(100) NOT NULL,
-  `Type` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`AddressID`)
-  );
-    CREATE TABLE IF NOT EXISTS `crimes`.`crime_records` (
-  `CrimeID` int NOT NULL AUTO_INCREMENT,
-  `Crime_description` VARCHAR(300) NOT NULL,
-  `AreaID` int NOT NULL,
-  `Date` VARCHAR(300) NOT NULL,
-  `Time` VARCHAR(300) NOT NULL,
-  `Additional info` VARCHAR(300),
-  PRIMARY KEY (`CrimeID`)
+  `CriminalID`int NOT NULL AUTO_INCREMENT,
+  `CrimeID` int NOT NULL,
+  PRIMARY KEY (`CriminalID`),
+  CONSTRAINT `FKCriminalID`
+  FOREIGN KEY (`CriminalID`)
+  REFERENCES `crimes`.`People` (`Person_ID`),
+  CONSTRAINT `FKCrime_ID1`
+  FOREIGN KEY (`CrimeID`)
+  REFERENCES `crimes`.`Crime_records` (`CrimeID`)
   );
 
 
-  CREATE TABLE IF NOT EXISTS `crimes`.`Zones` (
-  `Zone_the_ID` int NOT NULL AUTO_INCREMENT,
-  `Zone` VARCHAR(16) NOT NULL,
-  PRIMARY KEY (`Zone_the_ID`)
-  );
-ALTER TABLE  `crimes`.`Criminals`
-ADD CONSTRAINT `fk_criminalID`
-FOREIGN KEY (`CriminalID`)
-REFERENCES `People`(`Person_ID`);
 
-ALTER TABLE  `crimes`.`Victims`
-ADD CONSTRAINT `fk_victimID`
-FOREIGN KEY (`VictimID`)
-REFERENCES `People`(`Person_ID`);
 
-ALTER TABLE  `crimes`.`People_Addresses`
-ADD CONSTRAINT `fk_addressID`
-FOREIGN KEY (`AddressID`)
-REFERENCES `People`(`Addrees_ID`);
 
-ALTER TABLE  `crimes`.`Crime_area`
-ADD CONSTRAINT `fk_Zone_ID`
-FOREIGN KEY (`Zone_ID`)
-REFERENCES `Zones`(`Zone_ID`);
 
-ALTER TABLE  `crimes`.`crime_records`
-ADD CONSTRAINT `fk_CrimeID1`
-FOREIGN KEY (`fk_crimeid`)
-REFERENCES `Criminals`(`CrimeID`);
 
-ALTER TABLE  `crimes`.`crime_records`
-ADD CONSTRAINT `fk_CrimeID2`
-FOREIGN KEY (`fk_crimeid2`)
-REFERENCES `Victims`(`CrimeID`);
-
-ALTER TABLE  `crimes`.`crime_records`
-ADD CONSTRAINT `fk_Crime_area_ID`
-FOREIGN KEY (`AreaID`)
-REFERENCES `Crime_area`(`Area_ID`);
 
 
 INSERT INTO `crimes`.`users`
